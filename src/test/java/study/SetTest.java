9b87d6439848b2e9811bd0a0b6313dea0bf376cc
Max Kim <bluuminn@gmail.com>
2021-07-13 01:14:18 +0900

    test: ParameterizedTest를 적용한 contains 테스트 작성

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -2,6 +2,8 @@ package study;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -23,5 +25,11 @@ public class SetTest {
     @Test
     void size() {
         assertThat(numbers.size()).isEqualTo(4);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4})
+    void contains(int i) {
+        assertThat(numbers.contains(i)).isTrue();
     }
 }
